5b9cd7b36d15c1a5370260f735a5b28ed930e8f8
khaledghaly <khaled.ghaly303@gmail.com>
2025-05-18 14:08:40 +0300

    ADD PWA

--- sw.js
+++ sw.js
@@ -1,12 +1,27 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
-  self.skipWaiting();
-});
+const assets = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./css/style.css",
+  "./css/bootstrap.min.css",
+  "./js/main.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated');
-});
+self.addEventListener("install", function (installEvent) {
+  installEvent.waitUntil(
+    caches.open("my-pwa").then(function (cache) {
+      cache.addAll(assets)
+    })
+  )
+})
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('fetch', function (fetchEvent) {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(function (res) {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
