d0829e007992f934e57ecfa4ea4fc43fc9daaf20
Khaled Ghaly <112872753+khaled-hassan1@users.noreply.github.com>
2025-05-18 17:34:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 const assets = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./css/style.css",
-  "./css/bootstrap.min.css",
-  "./js/main.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "/",
+  "index.html",
+  "app.js",
+  "css/style.css",
+  "css/bootstrap.min.css",
+  "js/main.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
 self.addEventListener("install", function (installEvent) {
